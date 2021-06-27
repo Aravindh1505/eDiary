8e8baab85133eb96e1ec17fe55f2490d32c56327
Aravindh <aravindh.samidurai@tagitmobile.com>
2021-06-27 22:33:03 +0530

    updated Connectivity class

--- app/src/main/java/com/edairy/utils/internet/Connectivity.kt
+++ app/src/main/java/com/edairy/utils/internet/Connectivity.kt
@@ -1,110 +1,85 @@
-package com.edairy.utils.internet;
+package com.edairy.utils.internet
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.telephony.TelephonyManager;
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.telephony.TelephonyManager
 
 /**
- * Check device's network connectivity and speed 
+ * Check device's network connectivity and speed
  * @author emil http://stackoverflow.com/users/220710/emil
- *
  */
-public class Connectivity {
-  
-	/**
-	 * Get the network info
-	 * @param context
-	 * @return
-	 */
-	public static NetworkInfo getNetworkInfo(Context context){
-	    ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-	    return cm.getActiveNetworkInfo();
-	}
-	
-	/**
-	 * Check if there is any connectivity
-	 * @param context
-	 * @return
-	 */
-	public static boolean isConnected(Context context){
-	    NetworkInfo info = Connectivity.getNetworkInfo(context);
-	    return (info != null && info.isConnected());
-	}
-	
-	/**
-	 * Check if there is any connectivity to a Wifi network
-	 * @param context
-	 * @return
-	 */
-	public static boolean isConnectedWifi(Context context){
-	    NetworkInfo info = Connectivity.getNetworkInfo(context);
-	    return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
-	}
-	
-	/**
-	 * Check if there is any connectivity to a mobile network
-	 * @param context
-	 * @return
-	 */
-	public static boolean isConnectedMobile(Context context){
-	    NetworkInfo info = Connectivity.getNetworkInfo(context);
-	    return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
-	}
-	
-	/**
-	 * Check if there is fast connectivity
-	 * @param context
-	 * @return
-	 */
-	public static boolean isConnectedFast(Context context){
-	    NetworkInfo info = Connectivity.getNetworkInfo(context);
-	    return (info != null && info.isConnected() && Connectivity.isConnectionFast(info.getType(),info.getSubtype()));
-	}
-	
-	/**
-	 * Check if the connection is fast
-	 * @param type
-	 * @param subType
-	 * @return
-	 */
-	public static boolean isConnectionFast(int type, int subType){
-		if(type==ConnectivityManager.TYPE_WIFI){
-			return true;
-		}else if(type==ConnectivityManager.TYPE_MOBILE){
-			switch(subType){
-			case TelephonyManager.NETWORK_TYPE_1xRTT: // ~ 50-100 kbps
-			case TelephonyManager.NETWORK_TYPE_CDMA: // ~ 14-64 kbps
-			case TelephonyManager.NETWORK_TYPE_EDGE: // ~ 50-100 kbps
-			case TelephonyManager.NETWORK_TYPE_GPRS: // ~ 100 kbps
-				return false; 
-			
-			case TelephonyManager.NETWORK_TYPE_EVDO_0: // ~ 400-1000 kbps			
-			case TelephonyManager.NETWORK_TYPE_EVDO_A: // ~ 600-1400 kbps
-			case TelephonyManager.NETWORK_TYPE_HSDPA: // ~ 2-14 Mbps
-			case TelephonyManager.NETWORK_TYPE_HSPA: // ~ 700-1700 kbps
-			case TelephonyManager.NETWORK_TYPE_HSUPA: // ~ 1-23 Mbps
-			case TelephonyManager.NETWORK_TYPE_UMTS: // ~ 400-7000 kbps
-				return true; 
-			/*
-			 * Above API level 7, make sure to set android:targetSdkVersion 
-			 * to appropriate level to use these
-			 */
-			case TelephonyManager.NETWORK_TYPE_EHRPD: // API level 11  // ~ 1-2 Mbps
-			case TelephonyManager.NETWORK_TYPE_EVDO_B: // API level 9 // ~ 5 Mbps
-			case TelephonyManager.NETWORK_TYPE_HSPAP: // API level 13 // ~ 10-20 Mbps
-			case TelephonyManager.NETWORK_TYPE_LTE: // API level 11 // ~ 10+ Mbps
-				return true; 
-			case TelephonyManager.NETWORK_TYPE_IDEN: // API level 8 // ~25 kbps 
-				return false; 
-			// Unknown
-			case TelephonyManager.NETWORK_TYPE_UNKNOWN:
-			default:
-				return false;
-			}
-		}else{
-			return false;
-		}
-	}
+object Connectivity {
+    /**
+     * Get the network info
+     * @param context
+     * @return
+     */
+    fun getNetworkInfo(context: Context): NetworkInfo? {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return cm.activeNetworkInfo
+    }
 
+    /**
+     * Check if there is any connectivity
+     * @param context
+     * @return
+     */
+    fun isConnected(context: Context): Boolean {
+        val info = getNetworkInfo(context)
+        return info != null && info.isConnected
+    }
+
+    /**
+     * Check if there is any connectivity to a Wifi network
+     * @param context
+     * @return
+     */
+    fun isConnectedWifi(context: Context): Boolean {
+        val info = getNetworkInfo(context)
+        return info != null && info.isConnected && info.type == ConnectivityManager.TYPE_WIFI
+    }
+
+    /**
+     * Check if there is any connectivity to a mobile network
+     * @param context
+     * @return
+     */
+    fun isConnectedMobile(context: Context): Boolean {
+        val info = getNetworkInfo(context)
+        return info != null && info.isConnected && info.type == ConnectivityManager.TYPE_MOBILE
+    }
+
+    /**
+     * Check if there is fast connectivity
+     * @param context
+     * @return
+     */
+    fun isConnectedFast(context: Context): Boolean {
+        val info = getNetworkInfo(context)
+        return info != null && info.isConnected && isConnectionFast(info.type, info.subtype)
+    }
+
+    /**
+     * Check if the connection is fast
+     * @param type
+     * @param subType
+     * @return
+     */
+    fun isConnectionFast(type: Int, subType: Int): Boolean {
+        return if (type == ConnectivityManager.TYPE_WIFI) {
+            true
+        } else if (type == ConnectivityManager.TYPE_MOBILE) {
+            when (subType) {
+                TelephonyManager.NETWORK_TYPE_1xRTT, TelephonyManager.NETWORK_TYPE_CDMA, TelephonyManager.NETWORK_TYPE_EDGE, TelephonyManager.NETWORK_TYPE_GPRS -> false
+                TelephonyManager.NETWORK_TYPE_EVDO_0, TelephonyManager.NETWORK_TYPE_EVDO_A, TelephonyManager.NETWORK_TYPE_HSDPA, TelephonyManager.NETWORK_TYPE_HSPA, TelephonyManager.NETWORK_TYPE_HSUPA, TelephonyManager.NETWORK_TYPE_UMTS -> true
+                TelephonyManager.NETWORK_TYPE_EHRPD, TelephonyManager.NETWORK_TYPE_EVDO_B, TelephonyManager.NETWORK_TYPE_HSPAP, TelephonyManager.NETWORK_TYPE_LTE -> true
+                TelephonyManager.NETWORK_TYPE_IDEN -> false
+                TelephonyManager.NETWORK_TYPE_UNKNOWN -> false
+                else -> false
+            }
+        } else {
+            false
+        }
+    }
 }
